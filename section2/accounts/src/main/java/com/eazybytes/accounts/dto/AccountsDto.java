19370715659d7785d22e0bb22b050d605e7968e9
yurepires <contato.yurepires@gmail.com>
2025-08-02 18:44:01 -0300

    enhanced REST APIs documentation

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,21 +1,35 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 @Data
 public class AccountsDto {
 
+    @Schema(
+            description = "Account number of EazyBank account", example = "3454433243"
+    )
     @NotEmpty(message = "Account number can not be null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(
+            description = "Account type of EazyBank account", example = "Savings"
+    )
     @Column(name = "account_type")
     @NotEmpty(message = "Account type can not be null or empty")
     private String accountType;
 
+    @Schema(
+            description = "EazyBank branch address", example = "123 New York"
+    )
     @Column(name = "branch_address")
     @NotEmpty(message = "Branch address can not be null or empty")
     private String branchAddress;

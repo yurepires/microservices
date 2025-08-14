ebe37eea5d8e73861d1c6c9e47c137d0702c7707
yurepires <contato.yurepires@gmail.com>
2025-08-14 16:51:32 -0300

    Spring config server created

---
+++ section6/v2-spring-cloud-config/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -0,0 +1,43 @@
+package com.eazybytes.accounts;
+
+import com.eazybytes.accounts.dto.AccountsContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {AccountsContactInfoDto.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Accounts microservice REST API Documentation",
+                description = "EazyBank Accounts microservice REST API Documentation",
+                version = "v1",
+                contact = @Contact(
+                        name = "Yure Pires",
+                        email = "yurepires@example.com",
+                        url = "https://github.com/yurepires"
+                ),
+                license = @License(
+                name = "Apache 2.0",
+                url = "https://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "EazyBank Accounts microservice REST API Documentation",
+                url = "https://www.eazybytes.com/swagger-ui.html"
+        )
+)
+public class AccountsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsApplication.class, args);
+    }
+
+}

d5858a348c9341feb76a627a3798dcd5522ef5e2
yurepires <contato.yurepires@gmail.com>
2025-08-14 11:03:23 -0300

    Reading configurations using Environment interface in cards microservice

--- section6/v1-springboot/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ section6/v1-springboot/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -13,7 +13,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -37,6 +39,9 @@ public class CardsController {
 
     @Value("${build.version}")
     private String buildVersion;
+
+    @Autowired
+    public Environment environment;
 
     @Operation(
             summary = "Create Card REST API",
@@ -170,6 +175,30 @@ public class CardsController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(buildVersion);
+    }
+
+    @Operation(
+            summary = "Get java version",
+            description = "Get java versions details that is installed into cards microservice"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status Internal Server Error",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    })
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(environment.getProperty("JAVA_HOME"));
     }
 
 }

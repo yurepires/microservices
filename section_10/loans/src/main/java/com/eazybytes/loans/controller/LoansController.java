4a3683370a2e46b702c6f02a2da5551ca41fd0a7
yurepires <contato.yurepires@gmail.com>
2025-08-26 16:04:06 -0300

    Implementing retry pattern in Gateway Server

--- section_10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ section_10/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -232,6 +232,7 @@ public class LoansController {
     )
     @GetMapping("/contact-info")
     public ResponseEntity<LoansContactInfoDto> getContactInfo() {
+        logger.debug("Invoked Loans contact-info API");
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(loansContactInfoDto);

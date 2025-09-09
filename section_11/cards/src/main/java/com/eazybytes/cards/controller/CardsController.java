06fdaddb18b8911e164a6951aa633d34c2b8995e
yurepires <contato.yurepires@gmail.com>
2025-09-09 10:10:15 -0300

    Implementing Tracing using Grafana, Tempo & OpenTelemetry

--- section_11/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
+++ section_11/cards/src/main/java/com/eazybytes/cards/controller/CardsController.java
@@ -97,8 +97,9 @@ public class CardsController {
     public ResponseEntity<CardsDto> fetchCardDetails(
             @RequestHeader("eazybank-correlation-id") String correlationId,
             @RequestParam @Pattern(regexp="(^$|[0-9]{10})", message = "Mobile number must be 10 digits") String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        logger.debug("fetchCardDetails method start");
         CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        logger.debug("fetchCardDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
     }
 
--- section_11/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
+++ section_11/loans/src/main/java/com/eazybytes/loans/controller/LoansController.java
@@ -95,8 +95,9 @@ public class LoansController {
     public ResponseEntity<LoansDto> fetchLoanDetails(
             @RequestHeader("eazybank-correlation-id") String correlationId,
             @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits") String mobileNumber) {
-        logger.debug("eazyBank-correlation-id found: {}", correlationId);
+        logger.debug("fetchLoanDetails method start");
         LoansDto loansDto = iLoansService.fetchLoan(mobileNumber);
+        logger.debug("fetchLoanDetails method end");
         return ResponseEntity.status(HttpStatus.OK).body(loansDto);
     }
 
